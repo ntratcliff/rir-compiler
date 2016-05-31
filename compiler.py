e2d6ad5a768e18d3c93e55299e42dc3ed63341b5
Noah Ratcliff <me@nratcliff.com>
2016-05-31 12:21:26 -0400

    compile and save

--- compiler.py
+++ compiler.py
@@ -1,6 +1,21 @@
 import sys
+import os
 import re
 from PIL import Image
+
+def get_image_size(instructions):
+    return (len(instructions), 1) # TODO: output dimension parameters
+
+def hex2rgb(strhex):
+    r = int(strhex[:2], 16)
+    g = int(strhex[2:4], 16)
+    b = int(strhex[4:], 16)
+    return (r, g, b)
+
+def set_image_data(instructions, image):
+    for i in range(len(instructions)):
+        image[i, 0] = hex2rgb(instructions[i]) # TODO: output dimension param
+    return image
 
 # get ir filename and additional arguments
 flags = []
@@ -20,5 +35,15 @@ for i in range(len(instructions)):
     instructions[i] = re.sub(ur'( )|(;.*$)', '', instructions[i])
 
 instructions = [x for x in instructions if x != ""]
-
 print instructions
+
+# create Rainbow image
+image = Image.new("RGB", get_image_size(instructions), "black")
+pix = image.load()
+pix = set_image_data(instructions, pix)
+
+# write image file
+# TODO: output name param
+impath = os.path.splitext(irpath)[0]+'.bmp'
+print impath
+#image.save(impath)
