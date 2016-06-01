ba67e593d8581c512633ea39d3dcf57796d5de8d
Noah Ratcliff <me@nratcliff.com>
2016-06-01 00:11:48 -0400

    added width and height parameters

--- compiler.py
+++ compiler.py
@@ -1,11 +1,26 @@
 import sys
 import os
 import re
+import math
 import argparse
 from PIL import Image
 
 def get_image_size(instructions):
-    return (len(instructions), 1) # TODO: output dimension parameters
+    size = (len(instructions), 1)
+    if args.width is not None and args.height is not None:
+        # check if dimensions are valid
+        if args.width * args.height < len(instructions):
+            raise ValueError('The provided dimensions are too small')
+        size = (args.width, args.height)
+    elif args.width is not None:
+        # calculate minimum height
+        height = int(math.ceil(len(instructions) / float(args.width)))
+        size = (args.width, height)
+    elif args.height is not None:
+        # calculate minimum width
+        width = int(math.ceil(len(instructions) / float(args.height)))
+        size = (width, args.height)
+    return size
 
 def hex2rgb(strhex):
     r = int(strhex[:2], 16)
@@ -13,15 +28,29 @@ def hex2rgb(strhex):
     b = int(strhex[4:], 16)
     return (r, g, b)
 
-def set_image_data(instructions, image):
-    for i in range(len(instructions)):
-        image[i, 0] = hex2rgb(instructions[i]) # TODO: output dimension param
-    return image
+def set_image_data(instructions, pix, size):
+    rgb = []
+    for ins in instructions:
+        rgb.append(hex2rgb(ins))
+    return image_data_as_dim(rgb, pix, size)
+
+def image_data_as_dim(imdata, pix, size):
+    # pad blank areas of the image
+    for _ in range(len(imdata), size[0] * size[1]):
+        imdata.append((0,0,0))
+    imdata = [[imdata[size[0] * y + x] for x in range(size[0])] for y in range(size[1])]
+    for y in range(size[1]):
+        for x in range(size[0]):
+            pix[x, y] = imdata[y][x]
+    return pix
 
 # get ir filename and additional arguments
 parser = argparse.ArgumentParser(description='Compiler for the Rainbow Intermediate Representation.')
 parser.add_argument('rirpath', type=str, help='rir file to be compiled')
 parser.add_argument('--output', '-o', help='destination file for compiled Rainbow script')
+parser.add_argument('--dim-x', '-x', dest='width', type=int, help='output image width, in pixels')
+parser.add_argument('--dim-y', '-y', dest='height', type=int, help='output image height, in pixels')
+# TODO: Verbose
 
 args = parser.parse_args()
 
@@ -37,9 +66,10 @@ for i in range(len(instructions)):
 instructions = [x for x in instructions if x != ""]
 
 # create Rainbow image
-image = Image.new("RGB", get_image_size(instructions), "black")
+size = get_image_size(instructions)
+image = Image.new("RGB", size, "black")
 pix = image.load()
-pix = set_image_data(instructions, pix)
+pix = set_image_data(instructions, pix, size)
 
 # write image file
 if args.output is not None:
